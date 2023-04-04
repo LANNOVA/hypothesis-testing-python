c6ea54e696fddc1a141e1b7fb49f8ec221c56818
SRAVstudios <86397282+LANNOVA@users.noreply.github.com>
2023-04-04 15:31:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ from scipy import stats
  
 def normal_test(df):
     min_value = df['cosine_similarity'].min()
-    max_value = df['cosine_similarity'].max() # для расчета интервалов
+    max_value = df['cosine_similarity'].max() 
     print(df.head())
-    bin_count = int(np.ceil(np.log2(len(df))) + 1) # кол-во интервалов
+    bin_count = int(np.ceil(np.log2(len(df))) + 1)
     step = (max_value - min_value) / bin_count
     bins = [min_value]
-    for i in range(bin_count-1): # цикл расчета границ интервалов
+    for i in range(bin_count-1): 
         bins.append(bins[-1]+step)
     bins.append(max_value)
-    frequencies = pd.cut(df["cosine_similarity"], bins).value_counts(sort=False) # расчет частот
+    frequencies = pd.cut(df["cosine_similarity"], bins).value_counts(sort=False) 
     print(frequencies)
     return stats.normaltest(frequencies.to_numpy())
     
@@ -37,3 +37,15 @@ if pvalue <= significance_level:
     print('Reject NULL HYPOTHESIS') 
 else: 
     print('ACCEPT NULL HYPOTHESIS') 
+  
+ # DONATE SRAVstudios
+'''
+**BTC** - bc1q5kmqqynratseyh7v0n8q58rn7p5xejuemmc4px
+
+**USDT(ETH)**  - 0x8558288490E11E7F900471E7D52F0b0A0B6b8572
+
+**USDT(SOLANA)**  - 4MjmiAwiQT1cqb5fSpvdsKCabZAKxopcMsTqem9gWBqB
+
+**USDT(POLYGON)**  - 0x8558288490E11E7F900471E7D52F0b0A0B6b8572
+
+**ETH**  - 0x8558288490E11E7F900471E7D52F0b0A0B6b8572'''
